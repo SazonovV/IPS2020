2e585e4e5b15341f7588ba38ae0a283e0ac699a3
SazonovV <sazvlad1@gmail.com>
2020-05-14 19:20:49 +0300

    Fifth point of exercise
    №5

--- Source.cpp
+++ Source.cpp
@@ -15,18 +15,21 @@ double par(void)
     double S = 0.0;
     step = 1.0 / (double)num;
     double t = omp_get_wtime();
+    omp_lock_t writelock;
+    omp_init_lock(&writelock);
 
 #pragma omp parallel for
     for (i = 0; i < num; i++) {
         x = (i + 0.5) * step;
         S = S + 4.0 / (1.0 + x * x);
-#pragma omp critical
+        omp_set_lock(&writelock);
         inc++;
+        omp_unset_lock(&writelock);
     }
     t = omp_get_wtime() - t;
     pi = step * S;
     printf("Par: pi = %.14f\n", pi);
-
+    omp_destroy_lock(&writelock);
     return t;
 }
 
