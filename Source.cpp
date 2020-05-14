d333cf0479716f3a7da958ff3a57ec2526f2c1bd
SazonovV <sazvlad1@gmail.com>
2020-05-14 19:18:53 +0300

    Fourth point of exercise
    №4

--- Source.cpp
+++ Source.cpp
@@ -15,16 +15,18 @@ double par(void)
     double S = 0.0;
     step = 1.0 / (double)num;
     double t = omp_get_wtime();
+
 #pragma omp parallel for
     for (i = 0; i < num; i++) {
         x = (i + 0.5) * step;
         S = S + 4.0 / (1.0 + x * x);
-#pragma omp atomic
+#pragma omp critical
         inc++;
     }
     t = omp_get_wtime() - t;
     pi = step * S;
     printf("Par: pi = %.14f\n", pi);
+
     return t;
 }
 
